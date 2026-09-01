a0a9a15d07d643be1c88aca89f40e291e0569c0e
sumit-0522 <sumitkumer0522@gmail.com>
2026-09-01 11:56:32 +0530

    java

---
+++ day1/index.js
@@ -0,0 +1,27 @@
+const app =require('./app.js');
+//var a=10
+//var b=20
+//var c=20
+//console.log(a+b+c)
+
+// var x=20
+// if(x==20){
+    // console.log("x is 20")
+// }
+// else{
+    // console.log("x is not 20")
+// }
+//for(var i=0;i<5;i++){
+  //  console.log(i)}
+  //var x=20
+  //const arr =[1,2,3,4,5]
+  //for(var i=0;i<arr.length;i++){
+    //console.log(arr[i])
+  //}
+
+ // var a=5
+  //while(a--){
+   // console.log("Hello buddy!")
+  //}
+  console.log(app.xz);
+  console.log(app.yy);
