8455b460095681dc4f0f65696dcb2507241bef77
sumit-0522 <sumitkumer0522@gmail.com>
2026-09-15 12:05:35 +0530

    task

---
+++ task2.js
@@ -0,0 +1,15 @@
+let F="first"
+let L="last"
+let data={
+    [F]:"sumit",
+    [L]:"kasoudhan",
+
+Show(){
+    
+console.log(this.first)
+console.log(this.last)
+    
+}
+};
+console.log(data.Show())
+
