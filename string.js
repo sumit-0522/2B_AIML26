02721e4d98a457f80158e71ea66fb1daff196556
sumit-0522 <sumitkumer0522@gmail.com>
2026-09-15 12:04:46 +0530

    String

---
+++ string.js
@@ -0,0 +1,9 @@
+let First_name="sumit"
+let Last_name="kasoudhan"
+//console.log(`My First_name is ${First_name}`);
+//console.log(`"My name is:" ${First_name} ${Last_name}`);
+  function Fullname(first, last){
+    return `${first}${last}`
+  }
+  let Name=`Hello ${Fullname(First_name,Last_name)}`
+  console.log(Name)
